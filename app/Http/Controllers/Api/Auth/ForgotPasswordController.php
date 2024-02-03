a6afc89ce1e7d3cde3492ea228576ebce109ff00
MinHeinKoKo <kokominhein96@gmail.com>
2024-02-03 14:26:53 +0630

    making reset mail

--- app/Http/Controllers/Api/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Api/Auth/ForgotPasswordController.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\PasswordResetCode;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ForgotPasswordController extends Controller
 {
     public function sendResetMail(Request $request)
     {
-        $request->validate([
+        $data = $request->validate([
             "email" => "required|exists:users,email",
         ]);
 
         PasswordResetCode::where("email", $request->email)->delete();
         $code = mt_rand(100000 , 999999);
-
+        $data["code"] = $code;
+        $resetCode = PasswordResetCode::create($data);
+        Mail::to($request->email)->queue()
     }
 }
